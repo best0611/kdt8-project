352bfa84923a45d374a2462ee0ea25d602bdf57a
best0611 <best970611@naver.com>
2023-08-16 09:29:51 +0900

    project

---
+++ models/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const Sequelize = require("sequelize");
+const config = require(__dirname + "/../config/config.json")["development"];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+// 모델
+db.Student = require("./Students")(sequelize);
+db.Classes = require("./Classes")(sequelize);
+
+// 관계형성
+// 1대 다
+db.Student.hasMany(db.Classes, { foreignKey: "students_id" });
+db.Classes.belongsTo(db.Student, { foreignKey: "students_id" });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
